41aca3b72861dc470b32a2db509818b6e61bd600
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2023-01-06 12:11:38 +0100

    create and use customer middle ware to prevetn non application/json content-type for all route at application level

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import { bookRouter } from "./router/bookRouter";
 import { optionsJson, optionUrlEncode } from "./option";
+import { requestContentTypeJsonOnly } from "./middleware/OnlySupportApplicationJsonType";
 
 export const app: Application = express();
 export const port: string | number = process.env.PORT != null ? process.env.PORT : 3000;
 
 app.use(express.json(optionsJson));
 app.use(express.urlencoded(optionUrlEncode));
+app.use(requestContentTypeJsonOnly);
 app.use("/api/books", bookRouter);
 
 app.listen(port, () => {
