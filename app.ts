8c6e5c548d6ed7b061a8ab399ae72b23cf88dcb9
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2023-01-06 11:46:55 +0100

    added urlEncoded middleware for all route at application level

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
 import { bookRouter } from "./router/bookRouter";
-import { options } from "./option";
+import { optionsJson, optionUrlEncode } from "./option";
 
 export const app: Application = express();
 export const port: string | number = process.env.PORT != null ? process.env.PORT : 3000;
 
-app.use(express.json(options));
+app.use(express.json(optionsJson));
+app.use(express.urlencoded(optionUrlEncode));
 app.use("/api/books", bookRouter);
 
 app.listen(port, () => {
--- option.ts
+++ option.ts
@@ -1,5 +1,9 @@
-export const options = {
+export const optionsJson = {
     inflate: true,
     limit: '100kb',
     type: 'application/json'
   };
+
+  export const optionUrlEncode = {
+    extended: false
+  }
